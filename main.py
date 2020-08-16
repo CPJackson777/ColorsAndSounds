9e6ebc70ccd7ca3623e6b31f372bf6aeb9b06f62
Charles Jackson <cpjackson572@gmail.com>
2020-08-15 23:23:20 -0500

    comment

--- main.py
+++ main.py
@@ -10,4 +10,12 @@ from rezvani import MB
 # car4 = {"make": BMW, "color": "White", top_speed=120}
 # car5 = {"make": Subaru, "color": "Blue", top_speed=200}
 
-#
+# Move all common properties in your vehicles to a new Vehicle class.
+# class Vehicle:
+#   def __init__(self, make, color, top_speed, wheels=4):
+#     self.wheels = wheels
+#     self.make = make
+#     self.color = color
+#     self.top_speed = top_speed
+#   def drive(self):
+#     print(f"I'm driving in my {self.make} La la la la la")
