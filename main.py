9d6b84ea547871ed1ba9dc3006d5eb708887cc4d
Charles Jackson <cpjackson572@gmail.com>
2020-08-15 23:22:43 -0500

    added comment

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ from jaguar import Tesla
 from maserati import Zero
 from mercedes import Cessna
 from rezvani import MB
+
+# Define 5 of your favorite vehicles
+# car1 = {"make": Porsche, "color": "Black", top_speed=140 }
+# car2 = {"make": Jaguar, "color": "Gray", top_speed=100 }
+# car3 = {"make": Audi, "color": "Red", top_speed=110}
+# car4 = {"make": BMW, "color": "White", top_speed=120}
+# car5 = {"make": Subaru, "color": "Blue", top_speed=200}
+
+#
